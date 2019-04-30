e8a00f52b8fefb85af8e3f131b69a9f047a16798
Chris Hoayun <45009760+hoayun@users.noreply.github.com>
2019-04-30 14:39:15 -0400

    inital commit

---
+++ database/db.js
@@ -0,0 +1,10 @@
+const Sequelize = require("sequelize")
+const db = {}
+const sequelize = new Sequelize("lfg", "root", "root", {
+    host : "localhost",
+    dialect: "mysql",
+});
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db
---
+++ models/User.js
@@ -0,0 +1,7 @@
+const Sequelize = require("sequelize")
+const db = require("../database/db")
+
+module.exports = db.sequelize.define (
+    "user", {
+        
+    }
