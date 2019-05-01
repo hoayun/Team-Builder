0fdec4091120b0146629313b276455639ba42d93
Chris Hoayun <45009760+hoayun@users.noreply.github.com>
2019-05-01 11:23:57 -0400

    Backend Authentication done

--- models/User.js
+++ models/User.js
@@ -1,7 +1,25 @@
 const Sequelize = require("sequelize")
 const db = require("../database/db")
 
-module.exports = db.sequelize.define (
-    "user", {
-        
+module.exports = db.sequelize.define(
+  "user", {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    // The password cannot be null
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    screenname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
     }
+
+  })
