920cf5ea90e494b862b1507b24690c93a4e80ecd
Jeff Ambrosini <jeffambrosini@gmail.com>
2019-05-01 12:16:10 -0400

    modified pkg.json in client, modified app.js, installed pkgs

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					<Navbar />
+					<Route exact path="/" component={Landing} />
+					<div className="container">
+						<Route exact path="/register" component={Register} />
+						<Route exact path="/login" comoonent={Login} />
+						<Route exact path="profile" component={Profile} />
+					</div>
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
