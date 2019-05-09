dae8f17e9f9a6e2bb6db629503d11d20f575d40c
Chris Hoayun <45009760+hoayun@users.noreply.github.com>
2019-05-09 17:18:39 -0400

    players field added

--- client/src/components/Team.js
+++ client/src/components/Team.js
@@ -2,13 +2,22 @@ import React from "react";
 import "./Teamsfound.css";
 import { Card } from "react-materialize";
 
-function Team({ name, game, type, memo }) {
+function Team({ name, game, type, memo, players }) {
 	return (
 		<Card id="teamsfoundcard">
 			<h3 className="font-italic">{name}</h3>
 			{game && <h4 className="font-italic">{game}</h4>}
 			{type && <h4 className="font-italic">{type}</h4>}
 			{memo && <h4 className="font-italic">{memo}</h4>}
+			{players && <h4 className="font-italic">Current Players: {players}</h4>}
+			{<button className="btn waves-effect waves-light"
+                                id="joinbutton"
+                                type="submit"
+                                name="action"
+                                
+                            >
+                                Join <i className="fas fa-user-plus" />
+                            </button>}
 		</Card>
 	);
 }
